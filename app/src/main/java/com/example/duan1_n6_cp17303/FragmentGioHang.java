9cf776532fc8f42ddb8864cb85a27b63ff2afd0b
Quang <lamquang4112003@gmail.com>
2022-11-14 21:24:09 +0700

    update layout

---
+++ app/src/main/java/com/example/duan1_n6_cp17303/FragmentGioHang.java
@@ -0,0 +1,47 @@
+package com.example.duan1_n6_cp17303;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentGioHang extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public FragmentGioHang() {
+        // Required empty public constructor
+    }
+
+    public static FragmentGioHang newInstance(String param1, String param2) {
+        FragmentGioHang fragment = new FragmentGioHang();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gio_hang, container, false);
+    }
+}
